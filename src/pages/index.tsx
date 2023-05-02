394bdc9cb2c823e32de63541d25e65d17251651e
loique70 <fogouangloic10@gmail.com>
2023-05-02 02:22:41 -0700

    loique commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <main
+      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+    >
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+          Hello Loique wlcome to your firt deployed app
+          <h1>What's up there.....????.</h1>
+          <p>W are lionnes</p>
+      </div>
+    </main>
+  )
+}
